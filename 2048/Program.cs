3d41e9b881ad7685c9d184bc5d19692032b333af
Vince Vitale <VinceVitale@users.noreply.github.com>
2020-06-01 17:36:26 -0500

    Add files via upload

---
+++ 2048/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+
+namespace _2048
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game startNewGame;
+            int userInput = 0;
+            
+            Write("Enter 1 to continue or Enter 2 to start a new game: ");
+            int.TryParse(ReadLine(), out userInput);
+            if (userInput == 1)
+            {
+                startNewGame = new Game(false);
+            }
+            else
+            {
+                startNewGame = new Game(true);
+            }
+            startNewGame.Run();
+        }
+    }
+}
